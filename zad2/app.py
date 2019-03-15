418a10f4c73e1ab344073d4caeebfd65eda08701
Artur Przybysz <prz.artur@gmail.com>
2019-03-15 15:38:10 +0100

    signal comparison complet

--- zad2/app.py
+++ zad2/app.py
@@ -7,13 +7,16 @@ from zad1.signal_generators import sinusoidal_signal, gauss_noise, uniform_noise
 from zad1.plot import plot_signals, plot_histogram
 from zad2.quantization import quantization_with_rounding
 from zad2.sampling import sample_signal
-from zad2.similarity_measures import signal_to_noise_ratio, mean_squared_error
+from zad2.similarity_measures import signal_to_noise_ratio, mean_squared_error, peak_signal_to_noise_ratio, \
+    compare_signals
 
-s1 = sinusoidal_signal(amp=10, freq=2, duration=1.01, sampling_rate=20000)
+s1 = sinusoidal_signal(amp=10, freq=2, duration=1.01, sampling_rate=200)
 
-s2 = quantization_with_rounding(s1, bins=8)
+s_noisy1 = quantization_with_rounding(s1, bins=5)
+s_noisy2 = quantization_with_rounding(s1, bins=15)
+s_noisy3 = quantization_with_rounding(s1, bins=50)
+plot_signals(s1, s_noisy1)
 
-plot_signals(s1, s2)
-
-print(signal_to_noise_ratio(s1, s2))
-print(mean_squared_error(s1, s2))
+compare_signals(s1, s_noisy1)
+compare_signals(s1, s_noisy2)
+compare_signals(s1, s_noisy3)
--- zad2/similarity_measures.py
+++ zad2/similarity_measures.py
@@ -4,6 +4,13 @@ import numpy as np
 from zad1.signal_analysis import mean_power
 
 
+def compare_signals(pure_signal: Signal, noisy_signal: Signal):
+    print("Mean squared error:", np.round(mean_squared_error(pure_signal, noisy_signal), 3))
+    print("Signal to noise ratio:", np.round(signal_to_noise_ratio(pure_signal, noisy_signal), 3))
+    print("Peak signal to noise ratio:", np.round(peak_signal_to_noise_ratio(pure_signal, noisy_signal), 3))
+    print("Max difference:", np.round(max_difference(pure_signal, noisy_signal), 3))
+
+
 def mean_squared_error(s1: Signal, s2: Signal):
     return np.mean(np.power(s1.array - s2.array, 2))
 
@@ -14,3 +21,13 @@ def signal_to_noise_ratio(pure_signal: Signal, noisy_signal: Signal):
     p1 = mean_power(pure_signal)
     p2 = mean_power(Signal(noise, pure_signal.time, "", pure_signal.sampling_rate))
     return p1 / p2
+
+
+def peak_signal_to_noise_ratio(pure_signal: Signal, noisy_signal: Signal):
+    mse = mean_squared_error(pure_signal, noisy_signal)
+    psnr = 10 * np.log10((np.max(pure_signal.array) ** 2) / mse)
+    return psnr
+
+
+def max_difference(s1: Signal, s2: Signal):
+    return np.max(np.abs(s1.array - s2.array))
