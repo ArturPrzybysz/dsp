c74b938c89b4be4e6bc1fcd7ac5831b1703535e0
Artur Przybysz <prz.artur@gmail.com>
2019-03-15 15:54:36 +0100

    added bin analytics

---
+++ zad2/analytics.py
@@ -0,0 +1,41 @@
+import matplotlib.pylab as plt
+
+from zad1.Signal import Signal
+from zad2.quantization import quantization_with_rounding
+from zad2.similarity_measures import signal_to_noise_ratio, mean_squared_error, peak_signal_to_noise_ratio, \
+    max_difference
+
+plt.tight_layout()
+
+
+def analyze_sampling_params(bins, pure_signal: Signal):
+    mse_values = []
+    stn_values = []
+    pstn_values = []
+    md_values = []
+    for bin in bins:
+        quantized_signal = quantization_with_rounding(pure_signal, bins=bin)
+        mse_values.append(mean_squared_error(pure_signal, quantized_signal))
+        stn_values.append(signal_to_noise_ratio(pure_signal, quantized_signal))
+        pstn_values.append(peak_signal_to_noise_ratio(pure_signal, quantized_signal))
+        md_values.append(max_difference(pure_signal, quantized_signal))
+
+    plt.plot(bins, mse_values)
+    plt.xlabel("bin")
+    plt.ylabel("MSE(bin)")
+    plt.show()
+
+    plt.plot(bins, stn_values)
+    plt.xlabel("bin")
+    plt.ylabel("STN(bin)")
+    plt.show()
+
+    plt.plot(bins, pstn_values)
+    plt.xlabel("bin")
+    plt.ylabel("PSTN(bin)")
+    plt.show()
+
+    plt.plot(bins, md_values)
+    plt.xlabel("bin")
+    plt.ylabel("MD(bin)")
+    plt.show()
--- zad2/app.py
+++ zad2/app.py
@@ -5,18 +5,12 @@ from zad1.signal_generators import sinusoidal_signal, gauss_noise, uniform_noise
     rectangular_signal, unit_step_signal, impulse_signal, impulse_noise, rectangular_symmetrical_signal, triangle_wave
 
 from zad1.plot import plot_signals, plot_histogram
+from zad2.analytics import analyze_sampling_params
 from zad2.quantization import quantization_with_rounding
 from zad2.sampling import sample_signal
 from zad2.similarity_measures import signal_to_noise_ratio, mean_squared_error, peak_signal_to_noise_ratio, \
     compare_signals
 
-s1 = sinusoidal_signal(amp=10, freq=2, duration=1.01, sampling_rate=200)
+s1 = sinusoidal_signal(amp=10, freq=1, duration=1.01, sampling_rate=200)
 
-s_noisy1 = quantization_with_rounding(s1, bins=5)
-s_noisy2 = quantization_with_rounding(s1, bins=15)
-s_noisy3 = quantization_with_rounding(s1, bins=50)
-plot_signals(s1, s_noisy1)
-
-compare_signals(s1, s_noisy1)
-compare_signals(s1, s_noisy2)
-compare_signals(s1, s_noisy3)
+analyze_sampling_params(range(1, 100), s1)
