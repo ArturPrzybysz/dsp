29d6c9879e602c20fb86619112ab6ff5e87560bc
Artur Przybysz <prz.artur@gmail.com>
2019-05-10 22:18:55 +0200

    high pass, band pass

--- zad3/app.py
+++ zad3/app.py
@@ -5,34 +5,9 @@ from zad1.io_.writer import write_wav
 from zad1.signal_generators import sinusoidal_signal, gauss_noise
 from zad2.upsampling import sinc_resampling_ideal
 from zad3.convolution import convolve, naive_convolve
-from zad3.filter_generators import low_pass_filter
+from zad3.filter_generators import low_pass_filter, high_pass_filter, band_pass_filter
 from zad3.plot_spectrum import plot_spectra
 
-song = read_wav("bohemian_rhapsody.wav")
-song.array = song.array[:song.sampling_rate * 25]
-f0 = 1000
-low_pass1 = low_pass_filter(length=1551, f0=f0, sr=song.sampling_rate,window_type="hanning")
-# low_pass2 = low_pass_filter(length=151, f0=1000, sr=song.sampling_rate, window_type="hamming")
-# low_pass3 = low_pass_filter(length=151, f0=1000, sr=song.sampling_rate, window_type="hanning")
-
-song1 = convolve(song, low_pass1)
-# song2 = convolve(song, low_pass2)
-# song3 = convolve(song, low_pass3)
-
-plot_spectra(song)
-plot_spectra(song1)
-# plot_spectra(song2)
-# plot_spectra(song3)
-
-song1.array /= np.max(np.abs(song1.array), axis=0)
-
-write_wav(song1, "blackman" + str(f0) + "Hz.wav")
-# write_wav(song2, "hamming.wav")
-# write_wav(song3, "hanning.wav")
-
-# s2 = convolve(s1, f)
-# s3 = naive_convolve(s1, f)
-#
-# plot_signal(s1, scatter=True)
-# plot_signal(s2, scatter=True)
-# plot_signal(s3, scatter=True)
+s1 = gauss_noise(duration=5, mean=5, variance=10)
+f3 = band_pass_filter(length=101, f1=100, f2=200)
+plot_spectra(f3, mode="full")
--- zad3/filter_generators.py
+++ zad3/filter_generators.py
@@ -1,8 +1,10 @@
 import numpy as np
+from numpy import convolve
 
 from zad1.Signal import Signal
 from zad1.config import SR
 from zad3.Window import Window
+from zad3.plot_spectrum import plot_spectra
 
 
 def low_pass_filter(length: int, f0: int, sr: int = SR, window_type: str = "blackman"):
@@ -16,6 +18,30 @@ def low_pass_filter(length: int, f0: int, sr: int = SR, window_type: str = "blac
 
     return Signal(array=filter_array,
                   time=np.linspace(0, length / sr, num=length),
-                  name="(" + window_type + " filter)",
+                  name="(" + window_type + " low pass filter)",
                   sampling_rate=sr,
                   duration=length / sr)
+
+
+def high_pass_filter(length: int, f0: int, sr: int = SR, window_type: str = "blackman"):
+    _filter = low_pass_filter(length, sr // 2 - f0, sr, window_type)
+
+    _filter.array[1::2] *= -1
+    _filter.name.replace("low", "pass")
+
+    return _filter
+
+
+def band_pass_filter(length: int, f1: int, f2: int, sr: int = SR, window_type: str = "blackman"):
+    _low = low_pass_filter(length, f1, sr, window_type)
+    plot_spectra(_low)
+    _high = high_pass_filter(length, sr // 2 - f2, sr, window_type)
+    plot_spectra(_high)
+
+    band_pass_array = convolve(_low.array, _high.array)
+
+    return Signal(array=band_pass_array,
+                  time=np.linspace(0, band_pass_array / sr, num=length),
+                  name="(" + window_type + " band pass filter)",
+                  sampling_rate=sr,
+                  duration=band_pass_array / sr)
